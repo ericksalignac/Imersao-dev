b554f7d430af5ed03d594cbae829050ffae92970
ericksalignac <ericksalignac@gmail.com>
2021-09-22 09:45:27 -0300

    Aluraflix v1 concluido

---
+++ AluraFlix-v1/script.js
@@ -0,0 +1,17 @@
+var listaFilmes = [
+    "https://br.web.img3.acsta.net/medias/nmedia/18/91/79/19/20163665.jpg",
+    "https://br.web.img3.acsta.net/pictures/14/06/20/23/27/478475.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/thumb/5/53/The_Amazing_Spider-Man_Poster.jpg/250px-The_Amazing_Spider-Man_Poster.jpg",
+    "https://br.web.img2.acsta.net/medias/nmedia/18/91/31/05/20139182.jpg",
+    "https://br.web.img3.acsta.net/c_310_420/pictures/14/05/06/21/20/243550.jpg"
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
+  //document.write("<p>" + listaFilmes[0] + "</p>");
+  //document.write("<p>" + listaFilmes[1] + "</p>");
+  //document.write("<p>" + listaFilmes[2] + "</p>");
+  //document.write("<p>" + listaFilmes[3] + "</p>");
+  
